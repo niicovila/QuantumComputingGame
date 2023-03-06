5f820ebe8d3f20b4ed14fdb84a45053b6a3c71b6
Nicolas Vila <nicolasvila@uchicago.edu>
2023-03-06 13:45:58 -0600

    QCGame

---
+++ quantumcgame/src/Electron.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaBolt } from 'react-icons/fa';
+import './Electron.css';
+
+function Electron() {
+  return (
+    <div className="electron-container">
+      <div className="electron-icon"></div>
+    </div>
+  );
+}
+
+export default Electron;
